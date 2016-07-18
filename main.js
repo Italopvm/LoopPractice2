6e373e0840f55d7dcc115ddd633e2ce6e67a9114
italopvm <italopvm@gmail.com>
2016-07-17 19:26:50 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+var array = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot']
+
+
+//~~~~~~~~~~~~~~_______________ exercise 1. _______________~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+//  print this array = [rat cat butterfly marmot];
+
+// function animals(array){
+// 	array.pop();
+
+// 	console.log(array);
+// }
+// animals(array);
+
+
+
+//~~~~~~~~~~~~~~_______________ exercise 2. _______________~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+//  print this array = [rat butterfly ocelot];
+
+// function animals2(array){
+
+// 	for(var i = 0; i < array.length; i+=2){
+// 		console.log(array[i]);
+// 	}
+// }
+// animals2(array)
+
+//~~~~~~~~~~~~~~_______________ exercise 3. _______________~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+//  print this array = [ocelot marmot butterfly cat rat]
+
+// function animalsBackwards(array){
+// 	for(var i = array.length -1; i>=0; i--){
+// 		console.log(array[i])
+// 	};
+
+// };
+// animalsBackwards(array)
+
+
+
+//~~~~~~~~~~~~~~_______________ exercise 4. _______________~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+
+//   rat cat cat butterfly butterfly marmot marmot ocelot
+
+function animals3(array){
+	for(var i = 0; i < array.length; i++){
+		if(i=0){
+			console.log(array[i]);
+		}
+		if(i = ){
+			console.log(array[i])
+		}
+		else{
+			console.log(array[i]);
+			console.log(array[i]);
+		}
+	}
+}
+animals3(array);
+
+
+
+//~~~~~~~~~~~~~~_______________ exercise 4. _______________~~~~~~~~~~~~~~~~~~~~~~~~\\
